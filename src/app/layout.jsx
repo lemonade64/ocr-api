f99fca88b7a9a6c510dc34bfbdd8709b2ff5d0fd
Kevin Hou <strawberrypi64@gmail.com>
2024-07-02 10:20:21 +1000

    nextjs boilerplate

---
+++ src/app/layout.jsx
@@ -0,0 +1,56 @@
+import { Analytics } from "@vercel/analytics/react";
+import { Poppins } from "next/font/google";
+import "./globals.css";
+
+const poppins = Poppins({
+  subsets: ["latin"],
+  display: "swap",
+  variable: "--font-poppins",
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+});
+
+export const metadata = {
+  title: {
+    template: "OCR API - %s",
+    default: "OCR API",
+  },
+  description:
+    "API for extracting text from images encoded in base64 using Optical Character Recognition (OCR).",
+  category: "technology",
+  metadataBase: new URL(""),
+  openGraph: {
+    title: "OCR API",
+    description:
+      "API for extracting text from images encoded in base64 using Optical Character Recognition (OCR).",
+    url: "",
+    siteName: "OCR API",
+    locale: "en_AU",
+    type: "website",
+  },
+  robots: {
+    index: true,
+    follow: true,
+    nocache: true,
+    googleBot: {
+      index: true,
+      follow: false,
+      noimageindex: true,
+      "max-video-preview": -1,
+      "max-image-preview": "large",
+      "max-snippet": -1,
+    },
+  },
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <>
+      <html lang="en" className={poppins.className}>
+        <body>
+          {children}
+          <Analytics />
+        </body>
+      </html>
+    </>
+  );
+}
