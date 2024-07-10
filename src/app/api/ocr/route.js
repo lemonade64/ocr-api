4c3a24d227d32231220fb67c895da1e8850cf5ff
Kevin Hou <strawberrypi64@gmail.com>
2024-07-10 11:54:33 +1000

    this is getting out of hand...

--- src/app/api/ocr/route.js
+++ src/app/api/ocr/route.js
@@ -1,5 +1,7 @@
 import { NextResponse } from "next/server";
 import convertor from "@/lib/convertor";
+import getConfig from "next/config";
+import path from "path";
 import fs from "fs";
 
 const env = process.env.NODE_ENV;
@@ -13,7 +15,18 @@ export async function POST(req) {
   const base64Data = base64String.replace(/^data:image\/\w+;base64,/, "");
   const buffer = Buffer.from(base64Data, "base64");
   const imagePath =
-    env === "development" ? "./public/OCRImage.png" : "/tmp/OCRImage.png";
+    env === "development"
+      ? "./public/OCRImage.png"
+      : path.join(
+          getConfig().serverRuntimeConfig.PROJECT_ROOT,
+          ".next",
+          "server",
+          "app",
+          "api",
+          "ocr",
+          "OCRImage.png"
+        );
+  console.log(imagePath);
   fs.writeFile(imagePath, buffer, "base64", function (err) {
     if (err) {
       console.error(`Error writing image to ${imagePath}: ${err.message}`, err);
