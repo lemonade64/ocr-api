f155ba33aa28bc4421923a578aa6773f97b27403
Kevin Hou <strawberrypi64@gmail.com>
2024-07-10 12:04:14 +1000

    writeFileSync

--- src/app/api/ocr/route.js
+++ src/app/api/ocr/route.js
@@ -1,7 +1,5 @@
 import { NextResponse } from "next/server";
 import convertor from "@/lib/convertor";
-import getConfig from "next/config";
-import path from "path";
 import fs from "fs";
 
 const env = process.env.NODE_ENV;
@@ -15,19 +13,8 @@ export async function POST(req) {
   const base64Data = base64String.replace(/^data:image\/\w+;base64,/, "");
   const buffer = Buffer.from(base64Data, "base64");
   const imagePath =
-    env === "development"
-      ? "./public/OCRImage.png"
-      : path.join(
-          getConfig().serverRuntimeConfig.PROJECT_ROOT,
-          ".next",
-          "server",
-          "app",
-          "api",
-          "ocr",
-          "OCRImage.png"
-        );
-  console.log(imagePath);
-  fs.writeFile(imagePath, buffer, "base64", function (err) {
+    env === "development" ? "./public/OCRImage.png" : "/tmp/OCRImage.png";
+  fs.writeFileSync(imagePath, buffer, "base64", function (err) {
     if (err) {
       console.error(`Error writing image to ${imagePath}: ${err.message}`, err);
     }
